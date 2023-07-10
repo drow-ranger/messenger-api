dc987fbb30a1bdd0613b445e608368df391f8855
Deo Fibrianico <nico98deo@gmail.com>
2023-07-10 14:15:09 +0700

    first commit

---
+++ libs/shared/src/modules/redis.module.ts
@@ -0,0 +1,24 @@
+import { Module } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+
+import { redisStore } from 'cache-manager-redis-yet';
+
+import { RedisCacheService } from '../services/redis-cache.service';
+import { CacheModule } from '@nestjs/cache-manager';
+
+@Module({
+  imports: [
+    CacheModule.registerAsync({
+      useFactory: async (configService: ConfigService) => ({
+        store: await redisStore({
+          url: configService.get('REDIS_URI'),
+        }),
+      }),
+      isGlobal: true,
+      inject: [ConfigService],
+    }),
+  ],
+  providers: [RedisCacheService],
+  exports: [RedisCacheService],
+})
+export class RedisModule {}
